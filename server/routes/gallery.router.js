18820843dbbfaf48f4c30a89ea16c1f4b0f9dcff
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-02 17:40:46 -0500

    database-feature complete and functional

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
+
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
+    console.log(req.body);
+    likes=req.body.data*1;
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+    let sqlText = `UPDATE "list" SET "likes" = $1 WHERE id=$2;`;
+    //send the query to SQL
+    pool.query(sqlText, [likes, galleryId])
+    //if SQL doesn't error
+    .then( (result) => {
+        console.log('Like added')
+        res.sendStatus(200);
+    })
+    //if SQL DOES error
+    .catch( (error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })}); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+  const sqlText = `SELECT * FROM list ORDER BY id ASC;`;
+  pool.query(sqlText)
+      .then((result) => {
+          console.log(`Got stuff back from the database`, result);
+          res.send(result.rows);
+      })
+      .catch((error) => {
+          console.log(`Error making database query ${sqlText}`, error);
+          res.sendStatus(500); // Good server always responds
+      })
 }); // END GET Route
 
 module.exports = router;
