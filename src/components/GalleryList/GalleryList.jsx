b793afa8ffe8ed5bd85d35c111a93e6dc88168cd
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-02 13:27:15 -0500

    added comments to GalleryList

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,19 +1,23 @@
-import GalleryItem from '../GalleryItem/GalleryItem'
-import './GalleryList.css'
-
-
+//import component that we are iterating over using the map method
+import GalleryItem from "../GalleryItem/GalleryItem";
+//import the local styling so it shows up as a grid
+import "./GalleryList.css";
+//this function is invoked by the App.jsx
 function GalleryList(props) {
-  console.log(props);
+  //props includes the galleryList array and the addLike function
   return (
     <div>
       <h2>My Gallery</h2>
+      {/* grid styling applied to galleryWrapper */}
       <div className="galleryWrapper">
         {props.galleryList.map((item) => (
-                    <GalleryItem item={item} key={item.id} addLike={props.addLike} />
-          ))}
+          // the map method will iterate over the galleryList array, adding a GalleryItem component for each element in the array
+          <GalleryItem item={item} key={item.id} addLike={props.addLike} />
+          // GalleryItem needs a key and to pass down the addLike function and item object
+        ))}
       </div>
     </div>
   );
 }
-
+//export so App.jsx can call it
 export default GalleryList;
